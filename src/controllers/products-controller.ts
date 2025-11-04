3830320b571ec899d01f7c5baeecf2274ed28235
pedrohmf12 <pedrohenriferreira123@gmail.com>
2025-11-04 13:06:10 -0300

    Initial commit

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "@/utils/AppError.js";
+
+class ProductController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      //throw new AppError("Erro teste");
+
+      return response.json({ message: "ok" });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { ProductController };
